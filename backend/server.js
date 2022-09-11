75c7eaa9b0dc7b20caaf50ae18f4624da7146f9b
Shirzad Zeynal <z_shirzad@myself.com>
2022-09-11 19:10:43 +0430

    Static file serve added

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const colors = require('colors')
@@ -17,6 +18,16 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', goalRouter)
 app.use('/api/users', userRouter)
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  )
+}
 
 app.use(errorHandler)
 
